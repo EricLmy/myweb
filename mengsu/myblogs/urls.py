06b9cfde0d250637d8de9fc203e90c7d6e2d399e
lmy <lubuntu18.04.63@.com>
2019-01-21 11:24:59 +0800

    add error.html

--- mengsu/myblogs/urls.py
+++ mengsu/myblogs/urls.py
@@ -7,6 +7,7 @@ app_name = 'myblogs'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('login/', views.login, name="login"),
-	# path('<int:blogs_id>/comment/', views.comment, name="comment"),
+	path('<int:blogs_id>/', views.blogsdetail, name="blogsdetail"),
+	path('<int:blogs_id>/comment/', views.comment, name="comment"),
 
 ]
--- mengsu/myblogs/views.py
+++ mengsu/myblogs/views.py
@@ -1,17 +1,48 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
-from myblogs.models import User
+from myblogs.models import User, BlogsPost, BlogsComment
 
 def index(request):
 	return render(request, 'myblogs/index.html')
 
 def login(request):
-	print(request.POST['username'])
-	print(request.POST['pwd'])
-	user_list = User.objects.order_by('-user_time')
-	context = {'user_list':user_list}
-	uuu = get_object_or_404(User, user_name=request.POST['username'])
-	# uuu = User.objects.get(user_name=request.POST['username'])
-	print(uuu)
-	return render(request, 'myblogs/login.html', context)
+	user = get_object_or_404(User, user_name=request.POST['username'])
+	if user.user_pass == request.POST['pwd']:
+
+		blogs_list = BlogsPost.objects.filter(user_id=user.id)
+
+		# all_blogs_list = BlogsPost.objects.all()
+		# blogs_list = []
+		# for user_blog in all_blogs_list:
+		# 	if user_blog.user.user_name == request.POST['username']:
+		# 		blogs_list.append(user_blog)
+
+		context = {'blogs_list':blogs_list}
+		
+		return render(request, 'myblogs/login.html', context)
+	else:
+		return render(request, 'myblogs/error.html', {"error":"密码错误"})
+
+
+def blogsdetail(request, blogs_id):
+	blog = get_object_or_404(BlogsPost, pk=blogs_id)
+	try:
+		# blogs_comment = blog.blogscomment_set.order_by('-timestamp')
+		blogs_comment = blog.blogscomment_set.all()
+	except (KeyError, BlogsComment.DoesNotExist) as e:
+		return render(request, 'myblogs/error.html', {"error":"404"})
+
+	return render(request, 'myblogs/detail.html', {"blog":blog, "comment_list":blogs_comment})
+
+
+def comment(request, blogs_id):
+	blog = get_object_or_404(BlogsPost, pk=blogs_id)
+	try:#blogs_blogscomment.blog_id
+		comment = BlogsComment(blog=blog,comment_text=request.POST['comment'], goods=0, poors=0)
+		comment.save()
+	except Exception as e:
+		return render(request, 'myblogs/error.html', {"error": str(e)})
+
+	return HttpResponseRedirect(reverse('myblogs:blogsdetail',args=(blog.id,)))# 重定向
