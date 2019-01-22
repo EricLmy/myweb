560afcbcd15a8d3f06dca1aa4a8fd43b58c33868
lmy <lmy@lmy.com>
2019-01-22 22:34:37 +0800

    add blogs html and list blogs

--- mengsu/myblogs/urls.py
+++ mengsu/myblogs/urls.py
@@ -7,6 +7,10 @@ app_name = 'myblogs'
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('login/', views.login, name="login"),
+	path('login/<int:user_id>/', views.writeblog, name="writeblog"),
+	path('login/<int:user_id>/save/', views.saveblog, name="saveblog"),
+	path('register/register_fun/', views.userregister_fun, name="userregister_fun"),
+	path('register/', views.register, name="register"),
 	path('<int:blogs_id>/', views.blogsdetail, name="blogsdetail"),
 	path('<int:blogs_id>/comment/', views.comment, name="comment"),
 
--- mengsu/myblogs/views.py
+++ mengsu/myblogs/views.py
@@ -7,6 +7,21 @@ from myblogs.models import User, BlogsPost, BlogsComment
 def index(request):
 	return render(request, 'myblogs/index.html')
 
+def register(request):
+	return render(request, 'myblogs/register.html')
+
+def userregister_fun(request):
+	print(request.POST['username'])
+	print(request.POST['pwd'])
+	print(request.POST['mail'])
+	print(request.POST['datetime'])
+	try:
+		user = User(user_name=request.POST['username'],user_mail=request.POST['mail'], user_pass=request.POST['pwd'],user_time=request.POST['datetime'])
+		user.save()
+		return render(request, 'user/register.html', {'register_ok':"register success"})
+	except Exception as e:
+		return render(request, 'user/register.html', {'register_ok':str(e)})
+
 def login(request):
 	user = get_object_or_404(User, user_name=request.POST['username'])
 	if user.user_pass == request.POST['pwd']:
@@ -19,12 +34,23 @@ def login(request):
 		# 	if user_blog.user.user_name == request.POST['username']:
 		# 		blogs_list.append(user_blog)
 
-		context = {'blogs_list':blogs_list}
+		context = {'blogs_list':blogs_list, "user":user}
 		
 		return render(request, 'myblogs/login.html', context)
 	else:
 		return render(request, 'myblogs/error.html', {"error":"密码错误"})
 
+def writeblog(request, user_id):
+	return render(request, 'myblogs/blogdetail.html', {"userid":user_id})
+
+def saveblog(request, user_id):
+	user = get_object_or_404(User, pk=user_id)
+	try:
+		blog = BlogsPost(user=user, title=request.POST["title"], body=request.POST["body"], timestamp=request.POST["datetime"])
+		blog.save()
+	except Exception (KeyError, BlogsComment.DoesNotExist) as e:
+		return render(request, 'myblogs/error.html', {"error":"404"})
+	return HttpResponseRedirect(reverse('myblogs:blogsdetail',args=(blog.id,)))# 重定向 
 
 def blogsdetail(request, blogs_id):
 	blog = get_object_or_404(BlogsPost, pk=blogs_id)
